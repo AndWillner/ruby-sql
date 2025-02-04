efd67eecb06e0fa65daef128da50944c9cd9f432
Andrew Willner <andrew.willner@kellogg.northwestern.edu>
2025-02-04 22:25:32 +0000

    code-along 3-associations in class

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -12,6 +12,37 @@ Contact.destroy_all
 
 # 1. insert new rows in the contacts table with relationship to a company
 
+puts "There are #{Contact.all.count} contacts"
+
+apple = Company.find_by({"name" => "Apple"})
+#puts apple.inspect
+
+new_contact = Contact.new
+
+new_contact["first_name"] = "Tim"
+new_contact["last_name"] = "Cook"
+new_contact["email"] = "tim@apple.com"
+new_contact["company_id"] = apple["id"]
+
+puts new_contact.inspect
+
+new_contact.save
+puts "There are #{Contact.all.count} contacts"
+
+
 # 2. How many contacts work at Apple?
 
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+
+puts "Apple Employees: #{apple_contacts.count}"
+
+
 # 3. What is the full name of each contact who works at Apple?
+
+for contact in apple_contacts
+    first_name = contact["first_name"]
+    last_name = contact["last_name"]
+    name = "#{first_name} #{last_name}"
+    puts name
+end
+
